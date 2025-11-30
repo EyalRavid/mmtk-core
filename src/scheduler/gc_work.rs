53ff9b52104a6ee07b836792fe740c8817c6843f
Eyal Ravid <eyalravid2011@gmail.com>
2025-11-30 18:20:44 +0200

    enabled tracing for lxr

--- src/scheduler/gc_work.rs
+++ src/scheduler/gc_work.rs
@@ -1011,7 +1011,7 @@ pub trait ScanObjectsWork<VM: VMBinding>: GCWork<VM> + Sized {
         }
 
         #[cfg(feature = "tracing")]
-        probe!(mmtk, scan_objects, total_objects, scan_and_trace);
+        probe!(mmtk, scan_objects, 0, 0);
 
         // If any object does not support slot-enqueuing, we process them now.
         if !scan_later.is_empty() {
